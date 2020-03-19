27920d7c85f63e1ac1b2cef18964865e25078389
Nilesh Kant <nileshkant0@gmail.com>
2020-03-19 23:41:35 +0530

    Added multi account user schema

--- src/core/passport.js
+++ src/core/passport.js
@@ -1,8 +1,9 @@
 import passport from 'passport';
 import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
 
-import { SECRET } from '~/env';
-import { User } from '~/authentication/document';
+import { SECRET, AUTH } from '~/env';
+import { MultiAccountUser } from '../authentication/document';
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new JWTStrategy(
   {
@@ -13,12 +14,24 @@ passport.use(new JWTStrategy(
     try {
       if (Date.now() > jwtPayload.expires) return done('Token expired');
 
-      const user = await User.findOne({ username: jwtPayload.username }).exec();
+      const user = await MultiAccountUser.findOne({ username: jwtPayload.username }).exec();
       return done(null, user);
     } catch (error) {
       return done(error);
     }
   },
+));
+
+passport.use(new GoogleStrategy({
+    clientID: AUTH.GOOGLE.clientID,
+    clientSecret: AUTH.GOOGLE.clientSecret,
+    callbackURL: "https://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    MultiAccountUser.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
 ));
 
 export default passport;
